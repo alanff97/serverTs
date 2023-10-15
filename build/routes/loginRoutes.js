ec568897c1ee0301e9a3061d870e1f55b030ad41
alanff97 <alan.figueroa.cl@gmail.com>
2023-10-15 17:39:09 +0200

    some changes in login routes

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    else {
+        res.status(403);
+        res.send('Not permitted');
+    }
+}
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get('/login', (req, res) => {
@@ -51,4 +61,7 @@ router.post('/login', (req, res) => {
 router.get('/logout', (req, res) => {
     req.session = undefined;
     res.redirect('/');
+});
+router.get('/protected', requireAuth, (req, res) => {
+    res.send(`Welcome to protected information`);
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,8 +1,17 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  } else {
+    res.status(403);
+    res.send('Not permitted');
+  }
+}
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
@@ -56,5 +65,9 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
 router.get('/logout', (req: RequestWithBody, res: Response) => {
   req.session = undefined;
   res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send(`Welcome to protected information`);
 });
 export { router };
