aab130ed473f2b9ba8731fca9bccefe2d1283194
alanff97 <alan.figueroa.cl@gmail.com>
2023-11-07 20:56:43 +0100

    refact controllers and decorators

---
+++ src/controllers/decorators/controller.ts
@@ -0,0 +1,8 @@
+export function controller(routePrefix: string) {
+  return function (target: Function) {
+    Object.getOwnPropertyNames(target.prototype).forEach((key) => {
+      const routeHandler = target.prototype[key];
+      const path = Reflect.getMetadata('path', target.prototype, key);
+    });
+  };
+}
