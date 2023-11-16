668643ce55c5139edc222711e77d214166b416b7
alanff97 <alan.figueroa.cl@gmail.com>
2023-11-16 16:11:57 +0100

    finish the routes

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { get, controller } from './decorators';
+import { get, controller, bodyValidator, post } from './decorators';
+
 @controller('/auth')
 class LoginController {
   @get('/login')
-  
   getLogin(req: Request, res: Response): void {
     res.send(`
           <form method="POST">
@@ -18,5 +18,23 @@ class LoginController {
               <button>Submit</button>
           </form>
         `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email === 'hi@hi.com' && password === 'password') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password');
+    }
+  }
+  @get('/logout')
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect('/');
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
+
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import { AppRouter } from './AppRouter';
-
 import './controllers/loginController';
+import './controllers/rootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['sasdf'] }));
-app.use(router);
+
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
